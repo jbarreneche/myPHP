811502aef522e897e690093600468cf2eda87bdd
Juan Manuel Barreneche <snipper_me85@yahoo.com.ar>
2010-01-08 20:47:42 -0300

    Fixes default encoding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   include Authentication
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  after_filter :set_encoding
+
+protected
+
+  def set_encoding
+    headers['Content-Type'] ||= 'text/html'
+    if headers['Content-Type'].starts_with?('text/') and !headers['Content-Type'].include?('charset=')
+      headers['Content-Type'] += '; charset=utf-8'
+    end
+  end
 
 end
 
